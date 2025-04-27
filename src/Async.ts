63c905770491a7d4f0d5a29806801a63d506d0b7
Mihail Savostikov <mihail.savostikov@fruct.org>
2025-04-27 21:30:30 +0300

    Make task

--- src/Async.ts
+++ src/Async.ts
@@ -2,28 +2,20 @@
 Создайте функцию mock, которая принимает на вход аргумент number (количество миллисекунд) и возвращает Promise,
 который завершится через заданное количество миллисекунд со значением, переданным в аргумент.
  */
-export function mock(ms: number): Promise<number> {
+export function mock(time: number): Promise<number> {
+    return new Promise(resolve => setTimeout(() => resolve(time), time));
 }
 
 /*
 Перепишите функцию getData так, чтобы она выполнялась быстрее.
  */
-export function getData(): Promise<number[]> {
-    const result: number[] = [];
-
-    return mock(100)
-        .then((data1) => {
-            result.push(data1);
-            return mock(200);
-        })
-        .then((data2) => {
-            result.push(data2);
-            return mock(300);
-        })
-        .then((data3) => {
-            result.push(data3);
-            return result;
-        });
+export async function getData(): Promise<number[]> {
+    const [data1, data2, data3] = await Promise.all([
+        mock(100),
+        mock(200),
+        mock(300)
+    ]);
+    return [data1, data2, data3];
 }
 
 /*
@@ -32,8 +24,9 @@ export function getData(): Promise<number[]> {
  */
 export async function catchException(): Promise<string | undefined> {
     try {
-        Promise.reject(new Error('my error'));
+        await Promise.reject(new Error('my error'));
     } catch (err) {
-        return err.message;
+        return (err as Error).message;
     }
+    return;
 }
